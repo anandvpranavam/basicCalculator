04e23255dfbe45a70f238292b16507b9bb3339ab
Anand Venugopalapanicker <anandvpranavam@gmail.com>
2023-07-24 20:48:47 -0400

    added DEL button and its function to delete the numbers one by one using the toString(), split(),splice(), and join() methods combined

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const formEl = document.querySelector("form")
 
 
 const buttonsItem = document.querySelectorAll("button")
-
+let lastButtonData = 0;
 for(let i=0; i < buttonsItem.length; i++){
     buttonsItem[i].addEventListener("click", ()=>{
         const buttonData = buttonsItem[i].textContent;
@@ -16,7 +16,10 @@ for(let i=0; i < buttonsItem.length; i++){
             showResult();
         }
         else if(buttonData === "D"){
-            
+            const previousData = inputData.value;
+            // console.log(inputData.value);
+            // console.log(buttonData);
+            deleteData();
         }
         else{
             appendData(buttonData);
@@ -35,5 +38,14 @@ function showResult() {
 }
 function appendData(buttonData) {
     inputData.value = inputData.value + buttonData;
+    lastButtonData = inputData.value;
+}
+function deleteData(buttonData) {
+    // inputData.value = lastButtonData;
+    let currentvalue = inputData.value;
+    const y = currentvalue.toString().split('').slice(0, -1).join('');
+    const newNum = Number(y);
+    inputData.value = newNum;
+    console.log(inputData.value)
 }
 
